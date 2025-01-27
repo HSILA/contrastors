ed51a61ae8772b84010cac08c5d8d1dbc4c18e36
Ali Shiraee <a.shiraee@gmail.com>
2025-01-26 19:50:54 -0500

    add `base_dir` to `offset_count.py`, also save `offsets.json` as well

--- scripts/text/offsets_count.py
+++ scripts/text/offsets_count.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 def parser_args():
     parser = ArgumentParser()
     parser.add_argument("--data_dir", type=str, required=True)
+    parser.add_argument("--base_dir", type=str, default="contrastive")
 
     return parser.parse_args()
 
@@ -16,6 +17,7 @@ def parser_args():
 args = parser_args()
 
 data_dir = Path(args.data_dir)
+base_dir = Path(args.base_dir)
 
 files = sorted(data_dir.glob("shard-*.jsonl.gz"))
 
@@ -34,13 +36,16 @@ for file in tqdm(files):
             previous = end
             curr_count += 1
 
-    counts["count_per_file"][f"contrastive/{data_dir.name}/{file.name}"] = curr_count
-    offsets[f"contrastive/{data_dir.name}/{file.name}"] = idx2offset
+    counts["count_per_file"][f"{base_dir}/{data_dir.name}/{file.name}"] = curr_count
+    offsets[f"{base_dir}/{data_dir.name}/{file.name}"] = idx2offset
 
 
 with open(data_dir / "counts.json", "w") as f:
     counts["total_count"] = sum(counts["count_per_file"].values())
     json.dump(counts, f)
+
+with open(data_dir / "offsets.json", "w") as f:
+    json.dump(offsets, f)
 
 with gzip.open(data_dir / "offsets.json.gz", "wt") as f:
     json.dump(offsets, f)
