9970b59edc233b5cdcbf94fb44e789cf55dcd07b
Mohammad Khodadad <khodadadmohammad1997@gmail.com>
2025-04-29 19:15:16 +0000

    added freeze_used to config

--- src/contrastors/models/biencoder/configuration_biencoder.py
+++ src/contrastors/models/biencoder/configuration_biencoder.py
@@ -15,6 +15,7 @@ class BiEncoderConfig(PretrainedConfig):
         hamming=False,
         pretrained=False,
         gradient_checkpointing=False,
+        freeze_used=False,
         **kwargs,
     ):
         super().__init__(**kwargs)
@@ -29,3 +30,4 @@ class BiEncoderConfig(PretrainedConfig):
         self.hamming = hamming
         self.pretrained = pretrained
         self.gradient_checkpointing = gradient_checkpointing
+        self.freeze_used = freeze_used
